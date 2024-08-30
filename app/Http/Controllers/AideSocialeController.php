61ef1fd9a10998e8ed09409931e7c4e6b9bf427e
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-08-30 11:06:03 +0100

    Feat:Query and Tab for displaying the donators per social help appeal.

--- app/Http/Controllers/AideSocialeController.php
+++ app/Http/Controllers/AideSocialeController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Donateur;
 use App\Models\Participer;
 use App\Models\AideSociale;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreAideSocialeRequest;
 use App\Http\Requests\UpdateAideSocialeRequest;
 
 class AideSocialeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $socialhelps = AideSociale::latest()->get();
@@ -20,54 +18,42 @@ class AideSocialeController extends Controller
         return view('admin.social-help.index', compact('socialhelps'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreAideSocialeRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
         $socialhelp = AideSociale::find($id);
         $donations = Participer::where('numaid', $id)->get();
-        $donateurs = Donateur::whereHas('participers', function ($query) use ($id) {
-            $query->where('numaid', $id);
-        })->get();        
-return view('admin.social-help.show', compact('socialhelp', 'donations', 'donateurs'));
+
+        $donateurs = DB::table('participers')
+            ->join('aide_sociales', 'participers.numaid', '=', 'aide_sociales.numaid')
+            ->join('donateurs', 'participers.iddonat', '=', 'donateurs.iddonat')
+            ->where('aide_sociales.numaid', $id)
+            ->groupBy('donateurs.iddonat')
+            ->selectRaw('donateurs.*, SUM(participers.montantcontr) AS total_contributed')
+            ->get();
+
+        return view('admin.social-help.show', compact('socialhelp', 'donations', 'donateurs'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(AideSociale $aideSociale)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateAideSocialeRequest $request, AideSociale $aideSociale)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(AideSociale $aideSociale)
     {
         //
