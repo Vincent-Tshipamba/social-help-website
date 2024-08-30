20d9e7f8b5d927b9b679ffde1ef3114feef52de6
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-08-30 16:36:08 +0100

    feat: Improving the footer part

--- resources/views/partialsVisitor/footer.blade.php
+++ resources/views/partialsVisitor/footer.blade.php
@@ -5,9 +5,9 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer">
-                            <h2>About Us</h2>
-                            <p>Lorem ipsum dolor sit am consectetur adipisicing elit do eiusmod tempor incididunt ut
-                                labore dolore magna.</p>
+                            <h2> À propos de notre église</h2>
+                            <p>Nous sommes une communauté chrétienne dédiée à aider les personnes dans le besoin.
+                                Nous sommes reconnaissants pour votre soutien.</p>
                             <!-- Social -->
                             <ul class="social">
                                 <li><a href="#"><i class="icofont-facebook"></i></a></li>
@@ -21,34 +21,34 @@
                     </div>
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer f-link">
-                            <h2>Quick Links</h2>
+                            <h2>Liens utiles</h2>
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-12">
                                     <ul>
                                         <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Home</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>About Us</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Services</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Our
-                                                Cases</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Other Links</a></li>
+                                                    aria-hidden="true"></i>Accueil</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>À
+                                                propos de notre église</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Nos
+                                                ministères</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Nos
+                                                événements</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Faire
+                                                un don</a></li>
                                     </ul>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-12">
                                     <ul>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Notre
+                                                équipe</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Nos
+                                                partenaires</a></li>
                                         <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Consuling</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Finance</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Testimonials</a></li>
+                                                    aria-hidden="true"></i>Témoignages</a></li>
                                         <li><a href="#"><i class="fa fa-caret-right"
                                                     aria-hidden="true"></i>FAQ</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Contact Us</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Nous
+                                                contacter</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -56,23 +56,22 @@
                     </div>
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer">
-                            <h2>Open Hours</h2>
-                            <p>Lorem ipsum dolor sit ame consectetur adipisicing elit do eiusmod tempor incididunt.</p>
+                            <h2>Heures d'ouverture</h2>
+                            <p>Nous sommes ouverts du lundi au vendredi de 9h à 17h et le samedi de 10h à 14h.</p>
                             <ul class="time-sidual">
-                                <li class="day">Monday - Fridayp <span>8.00-20.00</span></li>
-                                <li class="day">Saturday <span>9.00-18.30</span></li>
-                                <li class="day">Monday - Thusday <span>9.00-15.00</span></li>
+                                <li class="day">Lundi - Vendredi <span>9.00-17.00</span></li>
+                                <li class="day">Samedi <span>10.00-14.00</span></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer">
-                            <h2>Newsletter</h2>
-                            <p>subscribe to our newsletter to get allour news in your inbox.. Lorem ipsum dolor sit
-                                amet, consectetur adipisicing elit,</p>
+                            <h2>Bulletin d'information</h2>
+                            <p>Inscrivez-vous à notre bulletin d'information pour recevoir toutes nos actualités dans
+                                votre boîte de réception.. Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
                             <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-                                <input name="email" placeholder="Email Address" class="common-input"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'"
+                                <input name="email" placeholder="Adresse e-mail" class="common-input"
+                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Votre adresse e-mail'"
                                     required="" type="email">
                                 <button class="button"><i class="icofont icofont-paper-plane"></i></button>
                             </form>
@@ -88,8 +87,8 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-12">
                         <div class="copyright-content">
-                            <p>© Copyright 2018 | All Rights Reserved by <a href="https://www.wpthemesgrid.com"
-                                    target="_blank">wpthemesgrid.com</a> </p>
+                            <p>© {{ date('Y') }} | Tous les droits réservés par <a
+                                    href="#" target="_blank">chapelle-des-vainqueurs.com</a> </p>
                         </div>
                     </div>
                 </div>
